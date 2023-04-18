378e6cf54336339137b1485d877071868af27db5
john <bblodfon@gmail.com>
2023-04-18 15:13:39 +0200

    add scripts for BLCA

---
+++ BLCA/data_split.R
@@ -0,0 +1,27 @@
+library(mlr3verse)
+library(mlr3proba)
+
+set.seed(42)
+
+tasks = readRDS(file = 'BLCA/data/tasks.rds')
+
+# verify all tasks have the same (time, status) outcomes
+for (tsk_index in 1:(length(tasks) - 1)) {
+  truth1 = tasks[[tsk_index]]$data(
+    cols = tasks[[tsk_index]]$target_names
+  )
+  truth2 = tasks[[tsk_index + 1]]$data(
+    cols = tasks[[tsk_index + 1]]$target_names
+  )
+  # ?all.equal.data.table
+  res = all.equal(truth1, truth2, check.attributes = FALSE)
+  if (class(res) == 'character') {
+    stop('Target values must be the same! ', res)
+  }
+}
+
+#' `mlr3proba:::partition.TaskSurv` is used by default which stratifies
+#' the survival task by `status`
+part = partition(tasks$Clinical, ratio = 0.8, stratify = TRUE)
+
+saveRDS(part, file = 'BLCA/data/part.rds')
---
+++ BLCA/efs.R
@@ -0,0 +1,85 @@
+#' => Ensemble feature selection on BLCA omics (tasks)
+#' => Use separately the C-index (OOB error) and RCLL measure to optimize
+#' during feature selection
+library(mlr3verse)
+library(survmob)
+library(tictoc)
+
+disease_code = 'BLCA'
+
+# Reproducibility
+set.seed(42)
+
+# Logging
+lgr::get_logger('bbotk')$set_threshold('warn')
+lgr::get_logger('mlr3' )$set_threshold('warn')
+
+# Tasks and data partition
+tasks = readRDS(file = paste0(disease_code, '/data/tasks.rds'))
+part  = readRDS(file = paste0(disease_code, '/data/part.rds'))
+
+# eFS variables
+nthreads_rsf = 10
+n_features = 5
+repeats = 100
+
+# helper function
+execute_efs = function(task, part, msr_id, rs, repeats, nthreads_rsf, n_features) {
+  message('Omic: ', task$id)
+
+  task2 = task$clone()
+  task2$filter(rows = part$train)
+  task2$col_roles$stratum = 'status' # Stratify
+
+  efs = eFS$new(msr_id = msr_id, resampling = rs, repeats = repeats,
+    nthreads_rsf = nthreads_rsf, n_features = n_features)
+  efs$run(task = task2)
+
+  efs
+}
+
+# Measure: OOB (1 - C-index) ----
+message('### OOB error ###')
+msr_id = 'oob_error'
+rs = rsmp('insample')
+
+data_list = list()
+for (task in tasks) {
+  if (task$id != 'Clinical') { # efs only for omic data
+    tic()
+    efs = execute_efs(task = task, part = part, msr_id = msr_id, rs = rs,
+      nthreads_rsf = nthreads_rsf, n_features = n_features, repeats = repeats
+    )
+    toc()
+
+    data_list[[task$id]] = efs
+
+    # temporary save just in case:
+    file_name = paste0(disease_code, '/efs/cindex_efs_', task$id, '.rds')
+    saveRDS(efs, file = file_name)
+  }
+}
+
+saveRDS(data_list, file = paste0(disease_code, '/efs/cindex_efs.rds'))
+
+# Measure: RCLL ----
+message('### RCLL ###')
+msr_id = 'rcll'
+rs = rsmp('cv', folds = 5)
+
+data_list = list()
+for (task in tasks) {
+  if (task$id != 'Clinical') { # efs only for omic data
+    efs = execute_efs(task = task, part = part, msr_id = msr_id, rs = rs,
+      nthreads_rsf = nthreads_rsf, n_features = n_features, repeats = repeats
+    )
+
+    data_list[[task$id]] = efs
+
+    # temporary save just in case:
+    file_name = paste0(disease_code, '/efs/rcll_efs_', task$id, '.rds')
+    saveRDS(efs, file = file_name)
+  }
+}
+
+saveRDS(data_list, file = paste0(disease_code, '/efs/rcll_efs.rds'))
