d3512fce019c55afce5f077f79d95b355bf5c915
john <bblodfon@gmail.com>
2023-05-16 12:09:55 +0200

    add multimodal task id and execute cmd
    * Increase default 'max-ppsize' due to memory issues

--- PAAD/efs_multimodal.R
+++ PAAD/efs_multimodal.R
@@ -1,6 +1,8 @@
 #' => Ensemble feature selection on all PAAD omics merged into a single
 #' multi-modal task
 #' => Use the C-index (OOB error) to optimize during feature selection
+#' => Execute command: `Rscript --max-ppsize=100000 PAAD/efs_multimodal.R`
+#' (aorsf fails with `protect(): protection stack overflow` for large datasets)
 library(mlr3verse)
 library(survmob)
 library(tictoc)
@@ -21,6 +23,7 @@ part  = readRDS(file = paste0(disease_code, '/data/part.rds'))
 tasks = readRDS(file = paste0(disease_code, '/data/tasks.rds'))
 po_featureunion = mlr3pipelines::po('featureunion')
 task_multimodal = po_featureunion$train(tasks)[[1L]]
+task_multimodal$id = paste0(mlr3misc::map_chr(tasks, `[[`, 'id'), collapse = '-')
 task_multimodal
 
 task_multimodal$filter(rows = part$train)
